d7690ae99f533d62b06be09719624d30d136ff11
Pavel Synek <pavel.synek@gmail.com>
2015-10-21 22:50:35 +0200

    order and product entity+dao placeholders

---
+++ backend/src/main/java/cz/muni/fi/pa165/deliveryservice/dao/OrderDaoImpl.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.deliveryservice.dao;
+
+/**
+ * Created by Pavel on 21. 10. 2015.
+ */
+
+public class OrderDaoImpl implements OrderDao {
+}
---
+++ backend/src/main/java/cz/muni/fi/pa165/deliveryservice/entity/Product.java
@@ -0,0 +1,18 @@
+package cz.muni.fi.pa165.deliveryservice.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by Pavel on 21. 10. 2015.
+ */
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
