24d4f6acc19dae57c71ccb5170dcc29f89101ead
tm <>
2015-10-27 16:07:18 +0100

    Interface and implementation of CustomerDao

---
+++ backend/src/main/java/cz/muni/fi/pa165/deliveryservice/dao/CustomerDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.pa165.deliveryservice.dao;
+
+import cz.muni.fi.pa165.deliveryservice.entity.Customer;
+
+/**
+ *
+ * @author Tomas Milota
+ */
+public interface CustomerDao extends PersonTemplate<Customer>{
+    
+}
