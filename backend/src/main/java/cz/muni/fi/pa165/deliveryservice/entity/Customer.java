11cf7759a44c488b6e50a517d000065d428ef7fb
tm <>
2015-10-27 12:44:58 +0100

    Customer entity added

---
+++ backend/src/main/java/cz/muni/fi/pa165/deliveryservice/entity/Customer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.pa165.deliveryservice.entity;
+
+/**
+ *
+ * @author Tomas Milota
+ */
+public class Customer extends DBPerson {
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Customer))
+            return false;
+        Customer other = (Customer) obj;
+        if (this.getEmail() == null){
+            if (other.getEmail() != null)
+                return false;
+        } else if (!this.getEmail().equals(other.getEmail()))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 29;
+        int result = 1;
+        result = prime * result + ((getEmail() == null) ? 0 : getEmail().hashCode());
+        return result;
+    }
+}
