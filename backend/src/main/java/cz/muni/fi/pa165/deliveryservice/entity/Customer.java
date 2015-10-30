4467246373b837d7ff9ddbbb2c4715cbeaf19d70
Matej Lesko <mlesko@redhat.com>
2015-10-30 21:15:10 +0100

    Customer now track his orders

--- backend/src/main/java/cz/muni/fi/pa165/deliveryservice/entity/Customer.java
+++ backend/src/main/java/cz/muni/fi/pa165/deliveryservice/entity/Customer.java
@@ -7,12 +7,20 @@
 package cz.muni.fi.pa165.deliveryservice.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Tomas Milota
  */
 @Entity
 public class Customer extends DBPerson {
+
+    @OneToMany
+    @NotNull
+    private List<Order> orders = new ArrayList<>();
 
     @Override
     public boolean equals(Object obj) {
