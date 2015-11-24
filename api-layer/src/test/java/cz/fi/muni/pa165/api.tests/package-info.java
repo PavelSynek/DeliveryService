0efbcfa3dec519479a5fb29b65ec60051daedde5
Matej Leško <GlobalDrvo@gmail.com>
2015-11-24 21:23:49 +0100

    UPDATE: packages initialization

---
+++ api-layer/src/test/java/cz/fi/muni/pa165/api.tests/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Matej Leško on 2015-11-24.
+ * Email: lesko.matej.pu@gmail.com, mlesko@redhat.com
+ * Phone: +421 949 478 066
+ * <p/>
+ * Project: delivery-service
+ */
+package cz.fi.muni.pa165.api.tests;
