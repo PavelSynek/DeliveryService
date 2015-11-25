f411e7431c2fa5513cc333dc56664323e786c710
Pavel Synek <pavel.synek@gmail.com>
2015-11-25 23:58:26 +0100

    simple ProductService

---
+++ service/src/main/java/cz/fi/muni/pa165/deliveryservice/ProductServiceImpl.java
@@ -0,0 +1,37 @@
+package cz.fi.muni.pa165.deliveryservice;
+
+import cz.muni.fi.pa165.deliveryservice.persist.dao.ProductDao;
+import cz.muni.fi.pa165.deliveryservice.persist.entity.Product;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Created by Pavel on 25. 11. 2015.
+ */
+public class ProductServiceImpl implements ProductService {
+
+    @Inject
+    private ProductDao productDao;
+
+    @Override
+    public Product createProduct(Product product) {
+        productDao.create(product);
+        return product;
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productDao.remove(findById(id));
+    }
+
+    @Override
+    public List<Product> findAllProducts() {
+        return productDao.findAll();
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return productDao.findById(id);
+    }
+}
