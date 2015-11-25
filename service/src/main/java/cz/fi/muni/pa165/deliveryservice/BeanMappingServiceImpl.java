4e8a578460142c67ff1dda73c9b8c92617a3c44f
Pavel Synek <pavel.synek@gmail.com>
2015-11-25 23:42:38 +0100

    added BeanMappingService

---
+++ service/src/main/java/cz/fi/muni/pa165/deliveryservice/BeanMappingServiceImpl.java
@@ -0,0 +1,35 @@
+package cz.fi.muni.pa165.deliveryservice;
+
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by Pavel on 25. 11. 2015.
+ */
+@Service
+public class BeanMappingServiceImpl implements BeanMappingService {
+
+    @Autowired
+    private Mapper dozer;
+
+    public <T> List<T> mapTo(Collection<?> objects, Class<T> targetClass) {
+        List<T> result = new ArrayList<>();
+        for (Object object : objects) {
+            result.add(mapTo(object, targetClass));
+        }
+        return result;
+    }
+
+    public <T> T mapTo(Object o, Class<T> targetClass) {
+        return dozer.map(o, targetClass);
+    }
+
+    public Mapper getMapper() {
+        return dozer;
+    }
+}
