f82cafbf1cc9aeb4b625941d3a3ce7e0290d40e5
TomasMilota <tommilota1@gmail.com>
2015-11-27 00:07:57 +0100

    NEW: PersonDTO

---
+++ api/src/main/java/cz/muni/fi/pa165/deliveryservice/dto/PersonDTO.java
@@ -0,0 +1,64 @@
+package cz.muni.fi.pa165.deliveryservice.dto;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Tomas Milota on 26.11.2015.
+ */
+public abstract class PersonDTO {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String surname;
+    private String phone;
+    private LocalDate registrationDate;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+}
