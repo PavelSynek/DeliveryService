de039b8b0b3a5938ae26a55ca2b163d33927f4d6
Matej Leško <GlobalDrvo@gmail.com>
2015-11-27 00:46:59 +0100

    NEW: exception for already shipped order created

---
+++ service/src/main/java/cz/muni/fi/pa165/deliveryservice/service/util/AlreadyShippedException.java
@@ -0,0 +1,19 @@
+package cz.muni.fi.pa165.deliveryservice.service.util;
+
+/**
+ * Created by Matej Leško on 2015-11-27.
+ * Email: lesko.matej.pu@gmail.com, mlesko@redhat.com
+ * Phone: +421 949 478 066
+ * <p>
+ * Project: DeliveryService
+ */
+
+/**
+ * Occurs when there is aan attempt to ship already shipped order
+ * @author Matej Leško
+ */
+public class AlreadyShippedException extends IllegalStateException {
+    public AlreadyShippedException(String desc) {
+        super(desc);
+    }
+}
