eb8dd75400f75653230e4a6d4fc1d5088da94290
Matej Leško <GlobalDrvo@gmail.com>
2015-11-27 07:19:46 +0100

    NEW: order DTO created

--- api/src/main/java/cz/muni/fi/pa165/deliveryservice/api/dto/OrderDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/deliveryservice/api/dto/OrderDTO.java
@@ -1,5 +1,11 @@
 package cz.muni.fi.pa165.deliveryservice.api.dto;
 
+import cz.muni.fi.pa165.deliveryservice.persist.enums.OrderState;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Matej Leško on 2015-11-27.
  * Email: lesko.matej.pu@gmail.com, mlesko@redhat.com
@@ -7,5 +13,94 @@ package cz.muni.fi.pa165.deliveryservice.api.dto;
  * <p>
  * Project: DeliveryService
  */
+
 public class OrderDTO {
+    private long id;
+    private CustomerDTO customer;
+    private EmployeeDTO employee;
+    private List<ProductDTO> products = new ArrayList<>();
+    private LocalDate created;
+    private OrderState state;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public CustomerDTO getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerDTO customer) {
+        this.customer = customer;
+    }
+
+    public EmployeeDTO getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(EmployeeDTO employee) {
+        this.employee = employee;
+    }
+
+    public List<ProductDTO> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductDTO> products) {
+        this.products = products;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
+    }
+
+    public OrderState getState() {
+        return state;
+    }
+
+    public void setState(OrderState state) {
+        this.state = state;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((created == null) ? 0 : created.hashCode());
+        result = prime * result + ((state == null) ? 0 : state.hashCode());
+        result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderDTO other = (OrderDTO) obj;
+        if (created == null) {
+            if (other.created != null)
+                return false;
+        } else if (!created.equals(other.created))
+            return false;
+        if (state != other.state)
+            return false;
+        if (customer == null) {
+            if (other.customer != null)
+                return false;
+        } else if (!customer.equals(other.customer))
+            return false;
+        return true;
+    }
 }
