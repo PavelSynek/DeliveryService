488652834e903db8bf077a30897e9677bd4cb843
Matej Leško <GlobalDrvo@gmail.com>
2015-11-27 14:47:38 +0100

    UPDATE: basic implementation of OrderCreateDTO added

--- api/src/main/java/cz/muni/fi/pa165/deliveryservice/api/dto/OrderCreateDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/deliveryservice/api/dto/OrderCreateDTO.java
@@ -8,9 +8,61 @@ package cz.muni.fi.pa165.deliveryservice.api.dto;
  * Project: DeliveryService
  */
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Matej Leško
  */
 public class OrderCreateDTO {
 
+    @NotNull
+    private CustomerDTO customer;
+
+    @NotEmpty
+    private List<ProductDTO> products = new ArrayList<>();
+
+    public CustomerDTO getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerDTO customer) {
+        this.customer = customer;
+    }
+
+    public List<ProductDTO> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductDTO> products) {
+        this.products = products;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OrderCreateDTO other = (OrderCreateDTO) obj;
+        if (customer == null) {
+            if (other.customer != null)
+                return false;
+        } else if (!customer.equals(other.customer))
+            return false;
+        return true;
+    }
 }
