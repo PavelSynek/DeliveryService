91761134eb25258289dfa11872b145724983c441
TomasMilota <tommilota1@gmail.com>
2015-11-27 06:52:06 +0100

    NEW: CustomerFacadeImpl

---
+++ service/src/main/java/cz/muni/fi/pa165/deliveryservice/service/facade/CustomerFacadeImpl.java
@@ -0,0 +1,57 @@
+package cz.muni.fi.pa165.deliveryservice.service.facade;
+
+import cz.muni.fi.pa165.deliveryservice.api.dto.CustomerDTO;
+import cz.muni.fi.pa165.deliveryservice.api.dto.PersonAuthenticateDTO;
+import cz.muni.fi.pa165.deliveryservice.api.facade.CustomerFacade;
+import cz.muni.fi.pa165.deliveryservice.persist.entity.Customer;
+import cz.muni.fi.pa165.deliveryservice.service.BeanMappingService;
+import cz.muni.fi.pa165.deliveryservice.service.CustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+
+/**
+ * Created by Tomas Milota on 27.11.2015.
+ */
+@Transactional
+@Service
+public class CustomerFacadeImpl implements CustomerFacade {
+
+    @Autowired
+    private CustomerServiceImpl customerService;
+
+    @Autowired
+    private BeanMappingService beanMappingService;
+
+    @Override
+    public CustomerDTO findById(Long customerId) {
+        Customer customer = customerService.findById(customerId);
+        return (customer == null) ? null : beanMappingService.mapTo(customer, CustomerDTO.class);
+    }
+
+    @Override
+    public CustomerDTO findByEmail(String email) {
+        Customer customer = customerService.findByEmail(email);
+        return (customer == null) ? null : beanMappingService.mapTo(customer, CustomerDTO.class);
+    }
+
+    @Override
+    public void create(CustomerDTO customerDTO, String unencryptedPassword) {
+        Customer customer = beanMappingService.mapTo(customerDTO, Customer.class);
+        customerService.create(customer, unencryptedPassword);
+        customerDTO.setId(customer.getId());
+    }
+
+    @Override
+    public Collection<CustomerDTO> getAll() {
+        return beanMappingService.mapTo(customerService.getAll(), CustomerDTO.class);
+    }
+
+    @Override
+    public boolean authenticate(PersonAuthenticateDTO p) {
+        Customer customer = customerService.findById(p.getPersonId());
+        return customerService.authenticate(customer, p.getPassword());
+    }
+}
