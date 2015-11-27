d09200a310b0a9a3c82bde206146e1497cb2c3b4
Matej Leško <GlobalDrvo@gmail.com>
2015-11-27 07:35:34 +0100

    NEW: exception representing violation of data access on DAO layer

---
+++ backend/src/main/java/cz/muni/fi/pa165/deliveryservice/persist/util/ViolentDataAccessException.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.pa165.deliveryservice.persist.util;
+
+import org.springframework.dao.DataAccessException;
+
+/**
+ * Created by Matej Leško on 2015-11-27.
+ * Email: lesko.matej.pu@gmail.com, mlesko@redhat.com
+ * Phone: +421 949 478 066
+ * <p>
+ * Project: DeliveryService
+ */
+
+/**
+ * Exception for DAO layer
+ *
+ * @author Matej Leško
+ */
+public class ViolentDataAccessException extends DataAccessException {
+    public ViolentDataAccessException(String message) {
+        super(message);
+    }
+}
