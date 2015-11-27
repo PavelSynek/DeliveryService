7c0173e201234eef17864fae452de1245a110352
Pavel Synek <pavel.synek@gmail.com>
2015-11-27 22:57:17 +0100

    empty ProductFacadeTest

---
+++ service/src/test/java/cz/muni/fi/pa165/deliveryservice/facade/tests/ProductFacadeTest.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pa165.deliveryservice.facade.tests;
+
+import org.testng.annotations.BeforeClass;
+
+/**
+ * Created by Pavel on 27. 11. 2015.
+ */
+public class ProductFacadeTest {
+
+    @BeforeClass
+    public void setUp() {
+
+    }
+}
