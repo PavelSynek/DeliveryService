5421ac6d7c01b2233d8e549ebe026ee65884cb36
Matej Leško <GlobalDrvo@gmail.com>
2015-11-27 07:36:26 +0100

    NEW: DTO for creating order - for now empty

---
+++ api/src/main/java/cz/muni/fi/pa165/deliveryservice/api/dto/OrderCreateDTO.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pa165.deliveryservice.api.dto;
+
+/**
+ * Created by Matej Leško on 2015-11-27.
+ * Email: lesko.matej.pu@gmail.com, mlesko@redhat.com
+ * Phone: +421 949 478 066
+ * <p>
+ * Project: DeliveryService
+ */
+
+/**
+ * @author Matej Leško
+ */
+public class OrderCreateDTO {
+
+}
