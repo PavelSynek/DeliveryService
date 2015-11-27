390538c38d780fdcc5230c8d974a88d1ec9c3d31
Matej Leško <GlobalDrvo@gmail.com>
2015-11-27 17:50:28 +0100

    NEW: implementation of order facade

---
+++ service/src/main/java/cz/muni/fi/pa165/deliveryservice/service/facade/OrderFacadeImpl.java
@@ -0,0 +1,11 @@
+package cz.muni.fi.pa165.deliveryservice.service.facade;
+
+/**
+ * Created by Matej Leško on 2015-11-27.
+ * Email: lesko.matej.pu@gmail.com, mlesko@redhat.com
+ * Phone: +421 949 478 066
+ * <p>
+ * Project: DeliveryService
+ */
+public class OrderFacadeImpl {
+}
