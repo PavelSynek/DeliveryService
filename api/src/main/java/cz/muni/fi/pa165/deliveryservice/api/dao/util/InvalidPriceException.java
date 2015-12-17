549a06537e52376099b6187f1e4b4624906c5d3a
Matej Leško <GlobalDrvo@gmail.com>
2015-12-17 14:59:41 +0100

    NEW: weight/price exception for dao layer created

---
+++ api/src/main/java/cz/muni/fi/pa165/deliveryservice/api/dao/util/InvalidPriceException.java
@@ -0,0 +1,18 @@
+package cz.muni.fi.pa165.deliveryservice.api.dao.util;
+
+/**
+ * Created by Matej Leško on 2015-12-17.
+ * Email: lesko.matej.pu@gmail.com, mlesko@redhat.com
+ * Phone: +421 949 478 066
+ * <p>
+ * Project: DeliveryService
+ */
+
+/**
+ * Throws during setting invalid price of the object into system
+ *
+ * @author Matej Leško
+ */
+public class InvalidPriceException extends Exception {
+
+}
