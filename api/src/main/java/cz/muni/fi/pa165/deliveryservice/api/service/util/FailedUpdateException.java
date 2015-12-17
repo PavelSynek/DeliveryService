9390474579613fd65f0bf03d2cd782d3e90cb183
Matej Lesko <mlesko@redhat.com>
2015-12-17 23:02:00 +0100

    NEW: exception for failing update added

---
+++ api/src/main/java/cz/muni/fi/pa165/deliveryservice/api/service/util/FailedUpdateException.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pa165.deliveryservice.api.service.util;
+
+/**
+ * Created by Matej Leško on 2015-12-17.
+ * Email: lesko.matej.pu@gmail.com, mlesko@redhat.com
+ * Phone: +421 949 478 066
+ * <p/>
+ * Project: delivery-service
+ */
+
+/**
+ * Exception raised during entity update problem
+ * @author Matej Leško
+ */
+public class FailedUpdateException extends Exception {
+
+}
