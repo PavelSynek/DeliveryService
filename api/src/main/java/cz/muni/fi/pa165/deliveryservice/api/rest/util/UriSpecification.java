d42ac0785f0f61fda4ec957001d993d7b7f4f83b
Matej Leško <GlobalDrvo@gmail.com>
2015-12-17 15:02:21 +0100

    NEW: uri specification for rest - TODO move in into another package

---
+++ api/src/main/java/cz/muni/fi/pa165/deliveryservice/api/rest/util/UriSpecification.java
@@ -0,0 +1,20 @@
+package cz.muni.fi.pa165.deliveryservice.api.rest.util;
+
+/**
+ * Created by Matej Leško on 2015-12-16.
+ * Email: lesko.matej.pu@gmail.com, mlesko@redhat.com
+ * Phone: +421 949 478 066
+ * <p>
+ * Project: DeliveryService
+ */
+
+/**
+ * Container of specific uris to provide REST control
+ * @author Matej Leško
+ */
+public final class UriSpecification {
+    public static final String ROOT_URI_PRODUCTS   = "/products";
+    public static final String ROOT_URI_USERS      = "/customers";
+    public static final String ROOT_URI_ORDERS     = "/orders";
+    public static final String ROOT_URI_EMPLOYEES = "/employees";
+}
