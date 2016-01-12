85e0092638daf252a6fc441452816134d1f7c4f7
Matej Lesko <mlesko@redhat.com>
2016-01-12 12:03:17 +0100

    NEW: initializer added to REST layer

---
+++ rest/src/main/java/cz/muni/fi/pa165/deliveryservice/rest/Initializer.java
@@ -0,0 +1,49 @@
+package cz.muni.fi.pa165.deliveryservice.rest;
+
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+ * @author Matej Leško
+ *
+ * Entry point for tomcat used for initialization and configuration
+ */
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootWebContext.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("utf-8");
+        encodingFilter.setForceEncoding(true);
+
+        ShallowEtagHeaderFilter shallowEtagHeaderFilter = new ShallowEtagHeaderFilter();
+
+        return new Filter[]{encodingFilter, shallowEtagHeaderFilter};
+    }
+
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws javax.servlet.ServletException {
+        super.onStartup(servletContext);
+        servletContext.addListener(RequestContextListener.class);
+    }
+
+}
